a44466b56abbc361194ef6935333da204aa2b5b6
kirankumar2403 <kirannallagutla24@gmail.com>
2026-02-10 10:43:52 +0530

    co

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div style={{ textAlign: "center", marginTop: "60px" }}>
-      <h1>Hello GitHub Actions + Vercel 🚀</h1>
+      <h1>Hello GitHub Actions</h1>
       <p>Version: 1</p>
     </div>
   );
